713636f78c282eef66418cd81eb3e0794ab3c89c
Damien <dubreuil.damien@laposte.net>
2020-04-10 17:26:25 +0200

    Ajout BDD

--- src/main/java/dao/interfaces/IFloreRepository.java
+++ src/main/java/dao/interfaces/IFloreRepository.java
@@ -1,10 +1,16 @@
 package dao.interfaces;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import model.Flore;
+import model.TypeCarac;
 
 
 public interface IFloreRepository extends JpaRepository<Flore, Long>{
-
+//	@Query("select fl from Flore fl join fl.referentielCaracteristique rc join rc.caracteristique c where c.typeCarac = :type AND c.nom= :nom AND c.valeur=:valeur")
+//	List<Flore> findAllByCaracteristique(@Param("type")TypeCarac typeCarac,@Param ("nom") String nom,@Param ("valeur") String valeur );
 }
